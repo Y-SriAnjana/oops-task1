f8ae9e9ab6f21054a43e8db5a5d4b5fb318dbb2c
Y-SriAnjana <yanamaddisrianjana@gmail.com>
2025-09-22 09:11:59 +0530

    Create PetSystem.java

---
+++ Inheritance/Inheritance/PetSystem.java
@@ -0,0 +1,31 @@
+package Inheritance;
+
+class Creature {
+	 void eat() {
+	     System.out.println("Creature is eating...");
+	 }
+	}
+
+	//Intermediate class
+	class Dog extends Creature {
+	 void bark() {
+	     System.out.println("Dog is barking...");
+	 }
+	}
+
+	//Derived class
+	class Puppy extends Dog {
+	 void weep() {
+	     System.out.println("Puppy is weeping...");
+	 }
+	}
+
+	//Main class
+	public class PetSystem {
+	 public static void main(String[] args) {
+	     Puppy puppy = new Puppy();
+	     puppy.eat();   // From Creature
+	     puppy.bark();  // From Dog
+	     puppy.weep();  // From Puppy
+	 }
+}
