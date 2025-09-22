a3dbe0cb9975ed8ef813cff50ed2d97add6b77b7
Y-SriAnjana <yanamaddisrianjana@gmail.com>
2025-09-22 13:32:17 +0530

    Create NumberFormatDemo.java

---
+++ ExceptionHandling/NumberFormatDemo.java
@@ -0,0 +1,18 @@
+package ExceptionHandling;
+
+import java.util.Scanner;
+
+public class NumberFormatDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        String input = sc.next();
+
+        try {
+            int num = Integer.parseInt(input);
+            System.out.println("Number: " + num);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Invalid number!");
+        }
+    }
+}
