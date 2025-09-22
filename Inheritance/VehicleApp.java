a09b51cac21c0c92314870bc7a7650a2a8074b36
Y-SriAnjana <yanamaddisrianjana@gmail.com>
2025-09-22 09:16:24 +0530

    Create VehicleApp.java

---
+++ Inheritance/VehicleApp.java
@@ -0,0 +1,22 @@
+package Inheritance;
+
+class Vehicle {
+	 void move() {
+	     System.out.println("Vehicle is moving...");
+	 }
+	}
+
+	//Derived class
+	class Car extends Vehicle {
+	 void fuelType() {
+	     System.out.println("Car uses petrol or diesel.");
+	 }
+	}
+
+	public class VehicleApp {
+	 public static void main(String[] args) {
+	     Car car = new Car();
+	     car.move();      // Inherited
+	     car.fuelType();  // Car-specific
+	 }
+	}
