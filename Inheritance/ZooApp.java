ed52a06877afdc8032247d120e1455995c13e0a0
Y-SriAnjana <yanamaddisrianjana@gmail.com>
2025-09-22 09:16:57 +0530

    Create ZooApp.java

---
+++ Inheritance/ZooApp.java
@@ -0,0 +1,22 @@
+package Inheritance;
+
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating...");
+    }
+}
+
+// Derived class
+class Lion extends Animal {
+    void roar() {
+        System.out.println("Lion is roaring...");
+    }
+}
+
+public class ZooApp {
+    public static void main(String[] args) {
+        Lion lion = new Lion();
+        lion.eat();   // Inherited behavior
+        lion.roar();  // Specific behavior
+    }
+}
