10202f867e8df11cb076992c94dfe004a168dda1
Y-SriAnjana <yanamaddisrianjana@gmail.com>
2025-09-22 09:40:40 +0530

    Create NullPointerCheck.java

---
+++ ExceptionHandling/NullPointerCheck.java
@@ -0,0 +1,12 @@
+package ExceptionHandling;
+
+public class NullPointerCheck {
+    public static void main(String[] args) {
+        String str = null;
+        try {
+            System.out.println("Length: " + str.length());
+        } catch (NullPointerException e) {
+            System.out.println("Error: String is null!");
+        }
+    }
+}
